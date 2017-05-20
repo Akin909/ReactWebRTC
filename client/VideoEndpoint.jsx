e6ddc3ac1de57b4fdedb180cdf5c8d6a39c41014
Akin909 <akin.sowemimo@gmail.com>
2017-05-20 22:30:11 +0100

    add styling [initial]

--- client/VideoEndpoint.jsx
+++ client/VideoEndpoint.jsx
@@ -2,21 +2,64 @@ import React from 'react';
 import { connect } from 'react-redux';
 import styled from 'styled-components';
 import { getUserVideoStream, addEndPoint } from './actions/index.js';
+import CSSTransitionGroup from 'react-transition-group/CSSTransitionGroup';
 import Endpoint from './Endpoint.jsx';
 
 const LocalCam = styled.video`
-  width: 10rem;
+  width: auto;
   height: 10rem;
   border: 0;
   box-shadow: 0 1px 1px grey;
 `;
 
-class VideoEndpoint extends Endpoint {
-  constructor(endpointName) {
-    super(endpointName);
-    this.props.addEndPoint(this);
-  }
+const LocalCamContainer = styled.div`
+  width: 15rem;
+  height: 15rem;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  align-items: center;
+  padding: 0.5rem;
+  z-index: 10;
+  left: 10%;
+  position: absolute;
+`;
+const RemoteCamContainer = styled.div`
+  width: 80%;
+  height: 60%;
+  display: flex;
+  align-items: center;
+  padding: 0.5rem;
+  flex-direction: column;
+  postition: relative;
+  box-shadow: 0 1px 1px rgba(0, 0, 0, 0.5),
+  0 1.5px 1.5px rgba(0, 0, 0, 0.5);
+`;
 
+const RemoteVideo = styled.video`
+  width: 100%;
+  height: 100%;
+  border: 0.2px solid black;
+  z-index: 0;
+`;
+
+const StartButton = styled.button`
+  width: 5rem;
+  border: 0;
+  padding: 0.4rem;
+  box-shadow: 0 1px 1px grey;
+`;
+
+const Container = styled.div`
+  width: 100%;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
+
+class VideoEndpoint extends Endpoint {
   /**
    * Send message call recieve on the target videoendpoint
    *
@@ -40,13 +83,31 @@ class VideoEndpoint extends Endpoint {
   };
 
   render() {
+    function FirstChild(props) {
+      const childrenArray = React.Children.toArray(props.children);
+      return childrenArray[0] || null;
+    }
     console.log(this.props.video);
     return (
-      <div>
-        {this.props.videoFeed &&
-          <LocalCam src={this.props.videoFeed} autoPlay="true" />}
-        <button onClick={this.sendMessage}>Start Call</button>
-      </div>
+      <Container>
+        <RemoteCamContainer>
+          <LocalCamContainer>
+            <CSSTransitionGroup
+              component={FirstChild}
+              transitionName="cam"
+              transitionEnterTimeout={500}
+              transitionAppear={true}
+              transitionAppearTimeout={500}
+              transitionLeaveTimeout={300}
+            >
+              {this.props.videoFeed &&
+                <LocalCam src={this.props.videoFeed} autoPlay="true" />}
+            </CSSTransitionGroup>
+          </LocalCamContainer>
+          <RemoteVideo />
+          <StartButton onClick={this.sendMessage}>Start Call</StartButton>
+        </RemoteCamContainer>
+      </Container>
     );
   }
 }
--- client/components/App.jsx
+++ client/components/App.jsx
@@ -2,9 +2,10 @@ import React, { Component } from 'react';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware, compose } from 'redux';
 import createSagaMiddleware from 'redux-saga';
+import styled, { injectGlobal } from 'styled-components';
 
 import reducer from './../reducers';
-import rtcSaga from './../Sagas/'
+import rtcSaga from './../Sagas/';
 import VideoEndpoint from './../VideoEndpoint.jsx';
 
 const defaultState = { endpoints: [] };
@@ -19,6 +20,47 @@ const store = createStore(
 );
 
 sagaMiddleware.run(rtcSaga);
+injectGlobal`
+  body, html {
+    padding: 0;
+    margin: 0;
+    width: 100%;
+    height: 100%;
+    font-family: 'Helvetica', sans-serif;
+    box-sizing: border-box;
+  }
+  * {
+   box-sizing: inherit;
+   font-family: inherit;
+  }
+  .cam-enter {
+    opacity: 0.01;
+  }
+  .cam-enter.example-enter-active {
+    opacity: 1;
+    transition: opacity 500ms ease-in;
+  }
+`;
+const AppContainer = styled.div`
+  width: 100%;
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  background-color: whitesmoke;
+`;
+
+const Title = styled.h1`
+  color:white;
+`;
+
+const Nav = styled.header`
+  width: 100%;
+  heigth: 2rem;
+  background-color:#0091EA;
+  text-align: center;
+`;
 
 class App extends Component {
   componentDidMount() {}
@@ -26,10 +68,12 @@ class App extends Component {
   render() {
     return (
       <Provider store={store}>
-        <div style={{ textAlign: 'center' }}>
-          <h1>WebRTC</h1>
+        <AppContainer>
+          <Nav>
+            <Title>WebRTC</Title>
+          </Nav>
           <VideoEndpoint />
-        </div>
+        </AppContainer>
       </Provider>
     );
   }
